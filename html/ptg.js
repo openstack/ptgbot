b8ca0c43ff196b6f61c9125ec006401c08293eab
Thierry Carrez <thierry@openstack.org>
2017-05-29 17:13:00 +0200

    Render hashtags using labels
    Use labels to represent #hashtags in the HTML rendering.
    Allows commands like "#swift now discussing #cinder" to
    render with two labels.

--- html/ptg.js
+++ html/ptg.js
@@ -4,6 +4,20 @@ var source = document.getElementById("PTGtemplate").innerHTML;
 // Handlebars compiles the above source into a template
 var template = Handlebars.compile(source);
 
+Handlebars.registerHelper('hashtag', function(options) {
+  var words = options.fn(this).split(" ");
+  var sentence = "";
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].startsWith("#")) {
+        sentence += '<span class="label label-info">'
+          + words[i].substring(1) + '</span> ';
+    } else {
+        sentence += words[i] + " ";
+    }
+  }
+  return new Handlebars.SafeString(sentence);
+});
+
 $.getJSON("ptg.json", function(json) {
   console.log(json);
   document.getElementById("PTGsessions").innerHTML = template(json);

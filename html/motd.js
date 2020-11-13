415ae5a05e2638ca4825850cdf84c862643798ca
Thierry Carrez <thierry@openstack.org>
2020-11-13 17:30:56 +0100

    Make URLs in MOTD messages clickable
    A long-wanted feature! Since the MOTD can only be controlled by an
    admin, the risk of abuse is very limited.
    
    Change-Id: Iff6263059d76c9b75ae26e029e3063992ab7cb76

--- html/motd.js
+++ html/motd.js
@@ -4,6 +4,14 @@ var dsource = document.getElementById("MOTDTemplate").innerHTML;
 // Handlebars compiles the above source into a template
 var dtemplate = Handlebars.compile(dsource);
 
+Handlebars.registerHelper('linkify', function(str) {
+    var pattern1 = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    var str1 = str.replace(pattern1, '<a target="_blank" href="$1">$1</a>');
+    var pattern2 =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    var str2 = str1.replace(pattern2, '$1<a target="_blank" href="http://$2">$2</a>');
+    return new Handlebars.SafeString(str2);
+});
+
 $.getJSON("ptg.json", function(json) {
   document.getElementById("MOTD").innerHTML = dtemplate(json);
 });
